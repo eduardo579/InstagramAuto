e9d0d5b64e4ef093a1cf3556fd3ee8d5ed86ec59
Eduardo579 <luiseduardogarciadepablo@protonmail.com>
2019-12-17 11:36:18 +0100

    commit

---
+++ setup_.py
@@ -0,0 +1,19 @@
+from post_list import PostList
+
+POST_HOUR = 12
+POST_DAYS = ['sunday', 'thursday', 'wednesday']
+POST_LIST = PostList([])
+DATES_OF_POSTS = {}
+
+USERNAME = "eduardo579"
+PASSWORD = "#Eduardogarcia579"
+
+DAYS_MAP = {
+    0: 'monday',
+    1: 'tuesday',
+    2: 'wednesday',
+    3: 'thursday',
+    4: 'friday',
+    5: 'saturday',
+    6 :'sunday',
+}
